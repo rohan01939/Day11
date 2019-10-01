6709941076a87596c7db59a0727b2c514c313807
rohan01939 <rohan01939@gmail.com>
2019-10-01 00:41:21 -0400

    day11

---
+++ src/Day11.java
@@ -0,0 +1,19 @@
+
+public class Day11 {
+public static void main(String[] args) {
+	String a="Rohan";
+	int b=10000;
+	byte c=26;
+	char d= 'y';
+	short e=23243;
+	long f=1234122l;
+	float g=33333333f;
+	double h=1.3443d;
+	boolean i=true;
+	
+	System.out.println(b+c+e+f);
+	
+	
+	System.out.println(a +" " + b+" " + c+" " + d+" " + e+" " + f + " " + g+" " + h+" " +i);
+}
+}
